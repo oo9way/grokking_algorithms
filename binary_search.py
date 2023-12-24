b599297d7147d68cf642168f8acbf31137c3e499
MuhammadYusuf Rakhimov <coderogabek@gmail.com>
2023-12-24 08:59:13 +0500

    Binary search algorithm

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,27 @@
 class BinarySearch:
+    """
+     Binary search algorithm | O(log n)
+
+    Arguments:
+        arr -- some kind of array
+    """
+
     def __init__(self, list) -> int:
         self.list = list
         self.iteration_count = 0
 
-    def find_item(self, item):
+    def find_item(self, item) -> [int, None]:
+        """
+        Finds given item from list using binary search
+
+        Arguments:
+            item -- int, float, str
+
+        Returns:
+            int - index of found item
+            None - when item is not found
+        """
+
         low = 0
         high = len(self.list) - 1
 
@@ -24,10 +42,23 @@ class BinarySearch:
         return None
 
     def get_iteration_count(self) -> int:
+        """
+        Get how many times iterated to search item
+
+        Returns:
+            int - count of iteration
+        """
+
         return self.iteration_count
 
 
+# * EXAMPLE
 search = BinarySearch([1, 3, 5, 6, 7, 9])  # set up list
 
-print(search.find_item(3))  # search item
-print(search.get_iteration_count())  # count how many times iteration works
+print("Found item index ", search.find_item(3))  # search item
+print("Iteration counter", search.get_iteration_count())  # count iterations
+
+
+# * RESULT
+# Found item index  1
+# Iteration counter 3
